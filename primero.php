c8558a539844c1ac8a7b8350ceefc3a63f71717f
DiegoViera <diegovieraskm49@gmail.com>
2018-08-27 21:03:11 -0300

    hello world

---
+++ primero.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+     <head>
+	      <meta charset="UTF-8">
+		  <title>pagina<\title>
+	 <head>
+	      <table border="1" width="100%">
+		       <tr>
+			     
+				  <th>1<\th>
+				  <th>2<\th>
+				  <th>3<\th>
+				  <th>4<\th>
